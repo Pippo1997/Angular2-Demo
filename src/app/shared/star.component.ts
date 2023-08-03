455149609f67808d30003fa0057796de989dc60e
filippo.sbaiz <sbaizfilippo@gmail.com>
2023-08-03 11:38:58 +0200

    Add product component, product- list and welcome

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, OnChanges, Output} from "@angular/core";
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: 'star.component.html',
+  styleUrls: ['star.component.css']
+})
+
+export class StarComponent implements OnChanges{
+  @Input() rating: number = 0;
+  cropWidth: number = 75;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
+
+  ngOnChanges() {
+    this.cropWidth = this.rating * 75/5;
+  }
+
+  onClick() {
+    this.ratingClicked.emit(`the rating is ${this.rating} was clicked`);
+  }
+
+
+}
