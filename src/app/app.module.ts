25693dd65e61f9fada24ec9dbc8344f53ff1d535
filippo.sbaiz <sbaizfilippo@gmail.com>
2023-08-03 11:37:02 +0200

    Add app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgOptimizedImage} from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
